e67f4cf9cf51f18eb8c3c5f33028b68be8627e15
Zathiald <A01705403@tec.mx>
2024-03-20 17:36:55 -0600

    Add new files to the repo

---
+++ hello.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo -e "\n"
+echo -e "Hola $1, bienvenido al curso.\n"
+echo -e "El script en ejecución es: $0\n"
+echo "El directorio actual es:"
+pwd
+echo -e "\n"
+echo -e "y su contenido es:\n"
+ls -la
+echo -e "\n"
